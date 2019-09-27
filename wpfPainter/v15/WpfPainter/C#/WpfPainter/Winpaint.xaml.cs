de0487281e492043394aadfb7980094a77e942ce
orenber <orenber@hotmail.com>
2019-09-27 04:10:55 +0300

    fix elipse shape bugs

--- wpfPainter/v15/WpfPainter/C#/WpfPainter/Winpaint.xaml.cs
+++ wpfPainter/v15/WpfPainter/C#/WpfPainter/Winpaint.xaml.cs
@@ -1,25 +1,12 @@
 ﻿using System;
-
-using System.Collections.Generic;
-
-using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.Windows.Threading;
 using System.IO;
-using System.Xaml;
 using System.Windows.Media.Animation;
-
-
-
-
 
 
 namespace WpfPainter
@@ -33,8 +20,6 @@ namespace WpfPainter
         {
             InitializeComponent();
         }
-
-
 
         #region File Menu
 
@@ -64,7 +49,8 @@ namespace WpfPainter
         #region Save Image
         private System.Drawing.Bitmap resultBitmap = null;
         private System.Drawing.Imaging.ImageFormat imgFormat;
-           private void butsave_Click(object sender, RoutedEventArgs e)
+
+        private void butsave_Click(object sender, RoutedEventArgs e)
         {
            System.Windows.Forms.SaveFileDialog SaveImageFile = new System.Windows.Forms.SaveFileDialog();
            SaveImageFile.Title = "Specify a file name and file path";
@@ -102,7 +88,6 @@ namespace WpfPainter
         #endregion
 
         private ImageBrush ib;
-
 
         // Open file image
         #region Open File image
@@ -177,7 +162,6 @@ namespace WpfPainter
            
         }
 
-
         private Point _startPoint;
         private Polyline _line;
 
@@ -187,8 +171,6 @@ namespace WpfPainter
             butrect.IsChecked = false;
             butcricle.IsChecked = false;
             butline.IsChecked = false;
-
-
         }
 
         #endregion
@@ -214,28 +196,28 @@ namespace WpfPainter
 
             eli.Fill = mybruse;
 
+            // create animation 
+            if (ani.IsChecked == true)
+            {
+                ani.Click += ani_Checked;
+            }
 
-            // Creat event to the new rect
-
-            eli.MouseLeftButtonDown += rect_MouseLeftButtonDown;
-            eli.MouseLeftButtonUp += rect_MouseLeftButtonUp;
-            eli.MouseMove += rect_MouseMove;
-
-
-
-
+            // add event to the new rect
+            Add_event_to_shape(eli);
             myCanvas.Children.Insert(0, eli);
 
-
-
-
-
         }
+
+        private Shape Add_event_to_shape(Shape object_Shape)
+        {
+            object_Shape.MouseLeftButtonDown += rect_MouseLeftButtonDown;
+            object_Shape.MouseLeftButtonUp += rect_MouseLeftButtonUp;
+            object_Shape.MouseMove += rect_MouseMove;
+            return object_Shape;
+        }
+
         #endregion
-
         #region Rectangle evants
-
-
 
         private void button2_Checked(object sender, RoutedEventArgs e)
         {
@@ -256,7 +238,6 @@ namespace WpfPainter
             rect.StrokeThickness = colorpiker.StrokeThickness;
             rect.Fill = myBrush;
 
-
            // creat animation 
 
             if (ani.IsChecked==true)
@@ -266,21 +247,13 @@ namespace WpfPainter
             }
 
             //  Creat event to the new rect
-            
-
-            rect.MouseLeftButtonDown += rect_MouseLeftButtonDown;
-            rect.MouseLeftButtonUp += rect_MouseLeftButtonUp;
-            rect.MouseMove += rect_MouseMove;
-
+            Add_event_to_shape(rect);
             myCanvas.Children.Insert(0, rect);
-
-
         }
         private bool _isRectDragInProg;
+
         private void rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
-
             try
             {
                 _isRectDragInProg = true;
@@ -288,12 +261,14 @@ namespace WpfPainter
                 {
                     Rectangle rect = sender as Rectangle;
                     rect.CaptureMouse();
+                    
                 }
-                else if (butcricle.IsChecked == true)
+                if (butcricle.IsChecked == true)
                 {
                     Ellipse eli = sender as Ellipse;
 
                     eli.CaptureMouse();
+                     
                 }
             }
             catch
@@ -372,27 +347,15 @@ namespace WpfPainter
 
         #endregion
 
-
-
         // slider evant -Select Size Shape
 
         #region Slider
 
-
-
-
         #endregion
-
 
         //  Color picker-Click the rectangle to select color
 
         #region Color picker
-
-
-
-
-
-     
 
         private void recblack_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
         {
@@ -401,11 +364,7 @@ namespace WpfPainter
 
         }
 
-
-
         #endregion
-
-
 
         // canvas-
         #region Canvas evant
@@ -433,20 +392,14 @@ namespace WpfPainter
             else if (e.RightButton == MouseButtonState.Pressed && MouseRightButtonDown==true )
             {
 
-
                 if (butrect.IsChecked == true)
                 {
                     shape = rect;
                 }
                 else if (butcricle.IsChecked == true)
                 {
-
                     shape = eli;
-
-                
                 }
-
-             
 
                 var x = startpoint.X;
                 var y = startpoint.Y;
@@ -457,26 +410,17 @@ namespace WpfPainter
                  shape.Width = Math.Abs(w);
                  shape.Height = Math.Abs(h);
 
-             
-
                 if (w < 0 && h < 0)
                 {
-
-
                     Canvas.SetRight(shape,myCanvas.Width- x);
-                    Canvas.SetTop(shape, y);
-                   
+                    Canvas.SetTop(shape, y);  
                 }
 
                 else if (w < 0 && h > 0)
                 {
-
-
                     Canvas.SetRight(shape,myCanvas.Width- x);
                     Canvas.SetBottom(shape,myCanvas.Height- y);
                     
-
-
                 }
 
                 else if (w > 0 && h > 0)
@@ -488,12 +432,8 @@ namespace WpfPainter
                 }
                 else if (w > 0 && h < 0)
                 {
-
-
-
                     Canvas.SetLeft(shape, x);
-                    Canvas.SetTop(shape, y);
-                    
+                    Canvas.SetTop(shape, y);   
                 }
                 else
                 {
@@ -505,62 +445,40 @@ namespace WpfPainter
             }
             else if (butline.IsChecked == true && e.LeftButton == MouseButtonState.Pressed)
             {
-
                 myline.X2 = curentpoint.X;
                 myline.Y2 = curentpoint.Y;
-
-
-            
             }
 
-
         }
-
 
         private Line myline;
         private void myCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
-
             _startPoint = e.GetPosition(myCanvas);
-
             _line = new Polyline();
             _line.Stroke = colorpiker.Fill;
-
-
-
-
             _line.StrokeThickness = colorpiker.StrokeThickness;
             myCanvas.Children.Add(_line);
-
 
             #region Line
 
             if (butline.IsChecked == true)
             {
-
                 startpoint = Mouse.GetPosition(myCanvas);
                 myline = new Line()
                 {
 
                     StrokeThickness = colorpiker.StrokeThickness,
-
                     Stroke = colorpiker.Fill,
                     X1 = startpoint.X,
                     Y1 = startpoint.Y
                 };
 
                 myCanvas.Children.Add(myline);
-            
-            
+
             }
 
-
-
-
             #endregion
-
-
         }
 
         //  When the mouse cursor entered into the canvas to select the tool varies depending on usage
@@ -577,21 +495,21 @@ namespace WpfPainter
                 myCanvas.Cursor = Cursors.Cross;
             }
         }
-
         private void myCanvas_MouseLeave(object sender, MouseEventArgs e)
         {
             xy.Content = string.Empty;
         }
+
         private void myCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
 
         }
 
-
         private bool MouseRightButtonDown;
         private Point startpoint;
         private Rectangle rect;
         private Ellipse eli;
+
         private void myCanvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
             if (butrect.IsChecked==true)
@@ -607,53 +525,42 @@ namespace WpfPainter
                     Fill = colorpiker.Fill
                 };
 
+                Add_event_to_shape(rect);
                 myCanvas.Children.Add(rect);
-
 
             }
 
-            else if (butcricle.IsChecked == true)
+            if (butcricle.IsChecked == true)
             {
 
                 MouseRightButtonDown = true;
 
                 startpoint = Mouse.GetPosition(myCanvas);
-                eli =new Ellipse()
+                eli = new Ellipse()
                 {
                     Stroke = colorpiker.Stroke,
                     StrokeThickness = colorpiker.StrokeThickness,
                     Fill = colorpiker.Fill
                 };
 
+                Add_event_to_shape(eli);
                 myCanvas.Children.Add(eli);
 
-
             }
-            else if (Pen.IsChecked == true)
+            if (Pen.IsChecked == true)
             {
 
                 _line.Stroke = colorpiker.Stroke;
 
-            
-            
             }
-
-
         }
 
         #endregion
-        
-   
 
         private void myCanvas_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
         {
             MouseRightButtonDown = false;
-           
-            eli = null;
         }
-
-
-
 
         #region Animation
 
@@ -661,35 +568,31 @@ namespace WpfPainter
         {
             if (ani.IsChecked == true)
             {
-
-                var da = new DoubleAnimation(360, 0, new Duration(TimeSpan.FromSeconds(1)));
-                var rt = new RotateTransform();
                         
                 if (butrect.IsChecked == true)
-                {
-
-                       
-                        rect.RenderTransform = rt;
-                        rect.RenderTransformOrigin = new Point(0.5, 0.5);
-                        da.RepeatBehavior = RepeatBehavior.Forever;
-                        rt.BeginAnimation(RotateTransform.AngleProperty, da);
-              
-
-
-
+                { 
+                    shape_animation(rect);
                 }
-            
-
-
+                if (butcricle.IsChecked == true)
+                {
+                    shape_animation(eli);
+                }
             }
+        }
 
+        private void shape_animation(Shape shape_object)
+        {
+            var da = new DoubleAnimation(360, 0, new Duration(TimeSpan.FromSeconds(1)));
+            var rt = new RotateTransform();
 
+            shape_object.RenderTransform = rt;
+            shape_object.RenderTransformOrigin = new Point(0.5, 0.5);
+            da.RepeatBehavior = RepeatBehavior.Forever;
+            rt.BeginAnimation(RotateTransform.AngleProperty, da);
 
         }
 
         #endregion
-
-       
 
         private void slider_strok_stikness_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
@@ -704,10 +607,6 @@ namespace WpfPainter
             butclear.IsChecked = false;
 
         }
-
-      
-
-      
 
         private void paintwin_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
@@ -728,8 +627,6 @@ namespace WpfPainter
 
         }
 
-        
-
         #region Color piker
 
         private void colorpiker_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -742,7 +639,6 @@ namespace WpfPainter
                 
             }
         }
-
 
         private void recColorRectangle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -801,8 +697,6 @@ namespace WpfPainter
 
         }
         #endregion
-
-
 
     }
 }
